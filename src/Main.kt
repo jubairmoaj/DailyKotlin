e50ea8cae7e3031fcef45a1748fadab09ff0df83
sayeedjoy <sayeedalmahmudjoy@gmail.com>
2025-07-01 13:50:33 +0600

    Initial commit

---
+++ src/Main.kt
@@ -0,0 +1,6 @@
+fun main() {
+    println("Cloudy")
+    println("Partly Cloudy"); println("Windy")
+    //have to use clone if want to write a multiple statement in same line
+    // or have to use different line
+}
