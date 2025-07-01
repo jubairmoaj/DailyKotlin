0cbc5b5c8b18954e27203dbe27f3c3369e575a39
sayeedjoy <sayeedalmahmudjoy@gmail.com>
2025-07-01 14:17:33 +0600

    v1

--- src/FirstProgram.kt
+++ src/FirstProgram.kt
@@ -1,6 +1,12 @@
 fun main() {
+    //title Print Something
     println("Cloudy")
     println("Partly Cloudy"); println("Windy")
     //have to use clone if want to write a multiple statement in same line
     // or have to use different line
+
+
+    //title Print Quotation \"
+    println("Say \"hello\"")
+
 }
---
+++ src/VariableDeclare.kt
@@ -0,0 +1,35 @@
+fun main() {
+    val count: Int = 2
+    println(count)
+
+
+    val name: String = "Jubair"
+    println(name)
+
+
+    val age: Int = 25;
+    print("$name age is $age")
+
+
+    //title-----------Operation inside print---------------
+    val earning: Int = 10000
+    val expense: Int = 5000
+
+
+    println("Earning: $earning")
+    println("Expense: $expense")
+
+    print("Remaining: ${earning - expense}")
+
+
+    //title-----------Without mention the variable type----------
+    val FirstName = "Jubair "
+    val SecondName = "Moaj"
+
+    val FullName = FirstName + SecondName
+
+    println("Hello $FullName")
+
+
+
+}
